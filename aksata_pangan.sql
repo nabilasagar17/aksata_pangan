361d3b814366993f6c2d4bc60fa8136a6c7346f4
nabilasagar17 <nabilaapriliasary@gmail.com>
2023-07-05 20:14:14 +0700

    @Nabila

--- aksata_pangan.sql
+++ aksata_pangan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 28, 2023 at 05:07 AM
+-- Generation Time: Jul 05, 2023 at 01:13 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -76,9 +76,11 @@ CREATE TABLE IF NOT EXISTS `penerima` (
 DROP TABLE IF EXISTS `stok_keluar`;
 CREATE TABLE IF NOT EXISTS `stok_keluar` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_penerima` int(11) DEFAULT NULL,
   `id_volunteer` int(11) DEFAULT NULL,
   `id_stok_masuk` int(11) DEFAULT NULL,
+  `nama_penerima` varchar(150) DEFAULT NULL,
+  `alamat_penerima` varchar(200) DEFAULT NULL,
+  `no_telp_penerima` varchar(15) DEFAULT NULL,
   `qty` decimal(10,0) DEFAULT NULL,
   `qty_sisa` decimal(10,0) DEFAULT NULL,
   `keperluan` varchar(30) DEFAULT NULL,
@@ -104,8 +106,12 @@ CREATE TABLE IF NOT EXISTS `stok_keluar` (
 DROP TABLE IF EXISTS `stok_masuk`;
 CREATE TABLE IF NOT EXISTS `stok_masuk` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pemberi` int(11) DEFAULT NULL,
   `nama` varchar(150) DEFAULT NULL,
+  `nama_pemberi` varchar(150) DEFAULT NULL,
+  `email_pemberi` varchar(100) DEFAULT NULL,
+  `no_telp_pemberi` varchar(15) DEFAULT NULL,
+  `alamat_pemberi` varchar(150) DEFAULT NULL,
+  `keterangan_pemberi` varchar(30) DEFAULT NULL,
   `kategori` varchar(50) DEFAULT NULL,
   `berat` decimal(10,0) DEFAULT NULL,
   `qty` decimal(10,0) DEFAULT NULL,
@@ -160,6 +166,37 @@ INSERT INTO `users` (`id`, `email`, `nama`, `alamat`, `telp`, `password`, `role_
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `view_stok_keluar`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `view_stok_keluar`;
+CREATE TABLE IF NOT EXISTS `view_stok_keluar` (
+`id` int(11)
+,`id_volunteer` int(11)
+,`id_stok_masuk` int(11)
+,`qty` decimal(10,0)
+,`qty_sisa` decimal(10,0)
+,`keperluan` varchar(30)
+,`tgl_masuk` datetime
+,`pengiriman` varchar(100)
+,`selesai` tinyint(1)
+,`keterangan` varchar(200)
+,`created_at` datetime
+,`created_by` varchar(100)
+,`updated_at` datetime
+,`updated_by` varchar(100)
+,`path_ttd` varchar(150)
+,`path_gambar` varchar(150)
+,`nama` varchar(150)
+,`exp_date` datetime
+,`kategori` varchar(50)
+,`nama_volunteer` varchar(150)
+,`no_telp_volunteer` varchar(15)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `volunteer`
 --
 
@@ -180,6 +217,16 @@ CREATE TABLE IF NOT EXISTS `volunteer` (
   `updated_by` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_stok_keluar`
+--
+DROP TABLE IF EXISTS `view_stok_keluar`;
+
+DROP VIEW IF EXISTS `view_stok_keluar`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_stok_keluar`  AS  select `stok_keluar`.`id` AS `id`,`stok_keluar`.`id_volunteer` AS `id_volunteer`,`stok_keluar`.`id_stok_masuk` AS `id_stok_masuk`,`stok_keluar`.`qty` AS `qty`,`stok_keluar`.`qty_sisa` AS `qty_sisa`,`stok_keluar`.`keperluan` AS `keperluan`,`stok_keluar`.`tgl_masuk` AS `tgl_masuk`,`stok_keluar`.`pengiriman` AS `pengiriman`,`stok_keluar`.`selesai` AS `selesai`,`stok_keluar`.`keterangan` AS `keterangan`,`stok_keluar`.`created_at` AS `created_at`,`stok_keluar`.`created_by` AS `created_by`,`stok_keluar`.`updated_at` AS `updated_at`,`stok_keluar`.`updated_by` AS `updated_by`,`stok_keluar`.`path_ttd` AS `path_ttd`,`stok_keluar`.`path_gambar` AS `path_gambar`,`stok_masuk`.`nama` AS `nama`,`stok_masuk`.`exp_date` AS `exp_date`,`stok_masuk`.`kategori` AS `kategori`,`volunteer`.`nama` AS `nama_volunteer`,`volunteer`.`no_telp` AS `no_telp_volunteer` from ((`stok_keluar` join `stok_masuk` on((`stok_masuk`.`id` = `stok_keluar`.`id_stok_masuk`))) join `volunteer` on((`volunteer`.`id` = `stok_keluar`.`id_volunteer`))) ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
